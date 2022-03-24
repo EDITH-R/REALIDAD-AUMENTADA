ac030baf2db9d58e447e8e34c66368724cafc91f
EDITH <rese-97@hotmail.com>
2022-03-24 12:35:59 -0600

    objetoss 3d

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="dropdown">
+  <button class="mainmenubtn">OBJETOS 3D</button>
+    <div class="dropdown-child">
+        <a href="recamara.html">Recámara</a>
+        <a href="sillon.html">Sillón</a>
+        <a href="cama.html">Cama</a>
+    </div>
+</div>
+</body>
+</html>
