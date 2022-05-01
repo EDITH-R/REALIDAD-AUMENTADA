774333ce2c19b5e3c801e0f2aba360f78bee6ca3
EDITH <rese-97@hotmail.com>
2022-04-30 20:31:31 -0500

    cambios en las urls

--- index.php
+++ index.php
@@ -8,10 +8,9 @@
 <div class="dropdown">
   <button class="mainmenubtn">OBJETOS 3D</button>
     <div class="dropdown-child">
-        <a href="https://objetos-3d.herokuapp.com/recamara.php">Recámara</a>
+        <a href="https://objetos-3d.herokuapp.com/mesa.php">Mesa</a>
+        <a href="https://objetos-3d.herokuapp.com/silla.php">Silla</a>
         <a href="https://objetos-3d.herokuapp.com/sillon.php">Sillón</a>
-        <a href="https://objetos-3d.herokuapp.com/cama.php">Cama</a>
-        <a href="https://objetos-3d.herokuapp.com/animado.php">Objeto Animado</a>
     </div>
 </div>
 </body>
--- silla.php
+++ silla.php
@@ -8,7 +8,7 @@
   <body>
     <a-scene embedded arjs>
       <a-entity scale=".02 .02 .02" > 
-        <a-entity gltf-model="https://objetos-3d.herokuapp.com/cama.glb" scale="2 2 2" crossOrigin="anonymous" >
+        <a-entity gltf-model="https://objetos-3d.herokuapp.com/silla.glb" scale="2 2 2" crossOrigin="anonymous" >
       </a-entity>
         <a-marker-camera preset='hiro'></a-marker-camera>
     </a-scene>
